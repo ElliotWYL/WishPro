8911deb793e5a3750a7e7e8204142df297cb1dc9
elliot.wang <elliot.wang@SHAGRMNTB11399>
2018-03-16 10:25:45 +0800

    Add test remote powershell script

---
+++ SuperPS/AutoDeploy/TestRemoteByHttps.ps1
@@ -0,0 +1,22 @@
+﻿#
+# Test remote server by https.
+# Output server name when remoting server
+#
+$account = "AD\xxx"
+$password = "00000000"
+$serverIP = "10.139.168.1"
+
+$securepwd = ConvertTo-SecureString $password -AsPlainText -Force
+$credential = New-Object System.Management.Automation.PSCredential -ArgumentList $account, $securepwd
+
+
+$session = New-PSSession -ComputerName "10.139.168.64" -Credential $credential -UseSSL
+
+Invoke-Command -Session $session -ThrottleLimit 1 -ScriptBlock {
+    # Todo on the remote server.
+
+	# Output remote server name
+    Write-Host -ForegroundColor Green (hostname)
+}
+
+Remove-PSSession $session
